06085d102a54fce008b996ba514d8f5f93b530bd
Naibitx <naibys.alzugarayofficial@gmail.com>
2026-02-24 06:35:36 -0500

    debuged memory.py and did implemented build long temr momry function, clesne dup other funcitons

--- memory.py
+++ memory.py
@@ -6,26 +6,25 @@ from langchain_community.vectorstores import Chroma
 # implementing short-term memory
 def short_memory(llm):
     return ConversationSummaryBufferMemory(
-    llm=llm
+    llm=llm,
     max_token_limit=500,
-    return_messages=True
-)
-
-embeddings = OpenAIEmbeddings()
-
-vector_store = Chroma(
-    persist_directory="./student_memory",
-    embedding_function=embeddings
-)
-
+    return_messages= True
+    )
 # storing long term memory
-def store_long_term_memory(text: str):
+def build_long_term_mem(embeddings):
+    return Chroma(
+        persist_directory="./user_memory",
+        embedding_function= embeddings
+    )
+def store_long_term_memory(vector_store, text: str):
     vector_store.add_texts([text])
     vector_store.persist()
 
 # retrieving long term memory
-def retrieve_long_term_memory(query: str, k: int = 3) -> str:
+def retrieve_long_term_memory(vector_store, query: str, k: int = 3) -> str:
     docs = vector_store.similarity_search(query=query, k=k)
+    if not docs:
+        return ""
     return "\n".join([doc.page_content for doc in docs])
 
 
